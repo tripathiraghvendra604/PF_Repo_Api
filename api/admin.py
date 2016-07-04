2a4930e80eaca414b1c7477e6159e7a016c2f84d
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-04 17:48:48 +0530

    admin section for achievementsimplemented

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import (UserInfo, EducationInfo, WorkExperience, Intrest, Skills, Certification, Publication,
-                     Patent, Books, Conference)
+                     Patent, Books, Conference, Achievement)
 
 admin.site.register(UserInfo)
 admin.site.register(EducationInfo)
@@ -14,3 +14,4 @@ admin.site.register(Publication)
 admin.site.register(Patent)
 admin.site.register(Books)
 admin.site.register(Conference)
+admin.site.register(Achievement)
