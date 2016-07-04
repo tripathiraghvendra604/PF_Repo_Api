1613eea52e02303762985ef2f15f72525a3b8a03
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-04 17:02:33 +0530

    admin section for books made

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import (UserInfo, EducationInfo, WorkExperience, Intrest, Skills, Certification, Publication,
-                     Patent)
+                     Patent, Books)
 
 admin.site.register(UserInfo)
 admin.site.register(EducationInfo)
@@ -12,3 +12,4 @@ admin.site.register(Skills)
 admin.site.register(Certification)
 admin.site.register(Publication)
 admin.site.register(Patent)
+admin.site.register(Books)
