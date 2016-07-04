c3fe689e5ebb7d8062a79aa91ba99c28f05ea098
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-04 17:31:10 +0530

    admin for conference made

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import (UserInfo, EducationInfo, WorkExperience, Intrest, Skills, Certification, Publication,
-                     Patent, Books)
+                     Patent, Books, Conference)
 
 admin.site.register(UserInfo)
 admin.site.register(EducationInfo)
@@ -13,3 +13,4 @@ admin.site.register(Certification)
 admin.site.register(Publication)
 admin.site.register(Patent)
 admin.site.register(Books)
+admin.site.register(Conference)
