71c05be2473ac6acbce1bea755770e26f21009e8
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-04 15:09:26 +0530

    models for Publication made

--- api/models.py
+++ api/models.py
@@ -99,3 +99,20 @@ class Certification(models.Model):
 
     def __unicode__(self):
         return self.user.username
+
+
+class Publication(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    # for national
+    year_national = models.TextField(null=True, blank=True)
+    journal_national = models.TextField(null=True, blank=True)
+    detail_national = models.TextField(null=True, blank=True)
+    status_national = models.TextField(null=True, blank=True)
+
+    # for international
+    year_international = models.TextField(null=True, blank=True)
+    journal_international = models.TextField(null=True, blank=True)
+    detail_international = models.TextField(null=True, blank=True)
+    status_international = models.TextField(null=True, blank=True)
+    
