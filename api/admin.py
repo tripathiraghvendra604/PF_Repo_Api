25c896d4a834f3e41c8b9d91cc4b3cedd139fcca
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-04 14:16:25 +0530

    admin control made

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserInfo, EducationInfo, WorkExperience, Intrest
+from .models import UserInfo, EducationInfo, WorkExperience, Intrest, Skills
 
 admin.site.register(UserInfo)
 admin.site.register(EducationInfo)
 admin.site.register(WorkExperience)
 admin.site.register(Intrest)
+admin.site.register(Skills)
