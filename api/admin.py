4600186c6b33f0d2e5f890ce2d58d481f6e3be78
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-04 15:00:11 +0530

    admin for Certification made

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserInfo, EducationInfo, WorkExperience, Intrest, Skills
+from .models import UserInfo, EducationInfo, WorkExperience, Intrest, Skills, Certification
 
 admin.site.register(UserInfo)
 admin.site.register(EducationInfo)
 admin.site.register(WorkExperience)
 admin.site.register(Intrest)
 admin.site.register(Skills)
+admin.site.register(Certification)
