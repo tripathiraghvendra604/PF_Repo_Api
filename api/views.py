6f6cb38d91bd73c413ceed3df6d28f4d3a4a4a22
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-07 11:35:16 +0530

    changes in registration

--- api/views.py
+++ api/views.py
@@ -65,14 +65,21 @@ class UserViewSet(CreateAPIView):
 
         if serializer.is_valid():
 
-            print request.data
+            email = request.data.get('email', None)
+            u = User.objects.filter(email=email).distinct()
+            if u.exists() or u.count() > 0:
+                return Response({
+                    'status': 'Bad Request',
+                    'message': 'This email is already registered!'
+                })
 
-            User.objects.create_user(**serializer.validated_data)
+            else:
+                User.objects.create_user(**serializer.validated_data)
 
-            return Response({
-                'status': 'Account Created',
-                'message': 'User Registered'
-            })
+                return Response({
+                    'status': 'Account Created',
+                    'message': 'User Registered'
+                })
 
         return Response({
             'status': 'Bad request',
