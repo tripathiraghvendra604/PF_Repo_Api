f1e58f9ac6b467d16535cea17de45e6117008702
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-09 12:05:15 +0530

    user object in extra curricular form

--- api/views.py
+++ api/views.py
@@ -515,7 +515,9 @@ class ExtraCurricularAPIView(CreateAPIView):
         })
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        user = serializer.validated_data['user']
+        user = get_object_or_404(User, username=user)
+        serializer.save(user=user)
 
 
 class SocialMediaLinksAPIView(CreateAPIView):
