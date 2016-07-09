7495f37040280eddc6e5decbab56bbf6c80c2c08
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-09 12:03:18 +0530

    user object in patent form

--- api/views.py
+++ api/views.py
@@ -404,7 +404,9 @@ class PatentAPIView(CreateAPIView):
         })
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        user = serializer.validated_data['user']
+        user = get_object_or_404(User, username=user)
+        serializer.save(user=user)
 
 
 class BookAPIView(CreateAPIView):
