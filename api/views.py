38605fcf58b603cdc4f4e943f63748fdaab8dffe
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-09 12:03:49 +0530

    user object in book form

--- api/views.py
+++ api/views.py
@@ -431,7 +431,9 @@ class BookAPIView(CreateAPIView):
         })
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        user = serializer.validated_data['user']
+        user = get_object_or_404(User, username=user)
+        serializer.save(user=user)
 
 
 class ConferenceAPIView(CreateAPIView):
