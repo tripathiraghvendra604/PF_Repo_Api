e3eca7509cd7c405769e14ac36b7f7b4b3485f72
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-11 12:07:42 +0530

    modifications accordingly in ConferenceSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -158,8 +158,8 @@ class ArticleSerializer(serializers.ModelSerializer):
 class ConferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conference
-        fields = ('year_c', 'org_c', 'detail_c', 'status_c',
-                  'year_i', 'org_i', 'detail_i', 'status_i', 'user')
+        fields = ('year_c', 'org_c', 'detail_c', 'status_c', 'title_c'
+                  'year_i', 'org_i', 'detail_i', 'status_i', 'title_i', 'user')
 
 
 class AchievementSerializer(serializers.ModelSerializer):
